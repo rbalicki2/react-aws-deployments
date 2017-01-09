a60d28e6bc82ab98c7163454675b0788d054dcff
Robert <robert.balicki@gmail.com>
2017-01-08 20:09:24 -0500

    moar

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,14 @@ if [ -z $DIST_HASH ]; then
   exit 1
 fi
 
+S3_BUCKET=aws-pres
+S3_BUCKET_FOLDER=$1
+
+if [ -z $S3_BUCKET_FOLDER ]; then
+  echo "***** S3_BUCKET_FOLDER is required. Bailing..."
+  exit 1
+fi
+
 aws s3 cp \
   s3://$S3_BUCKET/$S3_BUCKET_FOLDER/$DIST_HASH/index.html \
   s3://$S3_BUCKET/$S3_BUCKET_FOLDER/index.html \
