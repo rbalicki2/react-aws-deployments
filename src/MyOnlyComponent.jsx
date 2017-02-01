594811adb680c3987e941f31f96372507469679e
Robert <robert.balicki@gmail.com>
2017-02-01 17:50:33 -0500

    revert that

--- src/MyOnlyComponent.jsx
+++ src/MyOnlyComponent.jsx
@@ -7,7 +7,6 @@ export default class MyOnlyComponent extends Component {
   render() {
     return (<div>
       <h1>Super Smooth Deployments with React and AWS</h1>
-      <h2>Hello from QueensJS</h2>
       <ul>
         <li>This is the demo site that accompanies the presentation "Super Smooth Deployments with React and AWS".</li>
         <li>
